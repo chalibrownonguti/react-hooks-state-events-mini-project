146d7a3606308320407de37d9cf72b737af63548
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-05-10 16:50:12 +0300

    done

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-function Task() {
+function Task(text,task, category, click) {
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
+      {/* <div className="label">CATEGORY HERE</div> */}
       <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <button className="delete" onClick={()=> click(task)}>X</button>
     </div>
   );
 }
